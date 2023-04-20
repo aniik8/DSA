647a0a103217874e8839ac75ecdf5aa3c0621dff
Aniket <aniketmessi3@gmail.com>
2023-04-20 23:19:50 +0530

    First uppercase Letter In a string using recursion

--- src/RecursionEasy.java
+++ src/RecursionEasy.java
@@ -1,5 +1,16 @@
 public class RecursionEasy {
     public static void main(String[] args) {
-        System.out.println("Recursion");
+
+    }
+
+    // gfg practice
+    // First uppercase Letter In a string.
+    static char returnFirstUpper(String s){
+        if(s.length() == 0) {
+            return '0';
+        }
+        else if(Character.isUpperCase(s.charAt(0)))
+            return s.charAt(0);
+        else return returnFirstUpper(s.substring(1));
     }
 }
