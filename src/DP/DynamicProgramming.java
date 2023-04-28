1224e18d067e581163672db3689bf1dc854e4cf8
Aniket <aniketmessi3@gmail.com>
2023-04-28 23:13:41 +0530

    knapsack memoization

--- src/DP/DynamicProgramming.java
+++ src/DP/DynamicProgramming.java
@@ -10,10 +10,56 @@ public class DynamicProgramming {
         if(n == 0 || W == 0)
             return 0;
         if(wt[n-1] <= W){
-            return knapSack(W-wt[n-1], wt, val, n-1);
+            return Math.max(val[n-1]+knapSack(W-wt[n-1],wt,val,n-1),knapSack(W,wt,val,n-1));
+
         }
         else{
             return knapSack(W, wt, val, n-1);
         }
+    }
+    static int max(int a, int b) { return (a > b) ? a : b; }
+
+    // Returns the value of maximum profit
+    static int knapSackRec(int W, int wt[], int val[],
+                           int n, int[][] dp)
+    {
+
+        // Base condition
+        if (n == 0 || W == 0)
+            return 0;
+
+        if (dp[n][W] != -1)
+            return dp[n][W];
+
+        if (wt[n - 1] > W)
+
+            // Store the value of function call
+            // stack in table before return
+            return dp[n][W]
+                    = knapSackRec(W, wt, val, n - 1, dp);
+
+        else
+
+            // Return value of table after storing
+            return dp[n][W]
+                    = max((val[n - 1]
+                            + knapSackRec(W - wt[n - 1], wt, val,
+                            n - 1, dp)),
+                    knapSackRec(W, wt, val, n - 1, dp));
+    }
+
+    static int knapSacks(int W, int wt[], int val[], int N)
+    {
+
+        // Declare the table dynamically
+        int dp[][] = new int[N + 1][W + 1];
+
+        // Loop to initially filled the
+        // table with -1
+        for (int i = 0; i < N + 1; i++)
+            for (int j = 0; j < W + 1; j++)
+                dp[i][j] = -1;
+
+        return knapSackRec(W, wt, val, N, dp);
     }
 }
