5736b8e1c9662af9eddf06bb54e139730f108091
Aniket <aniketmessi3@gmail.com>
2023-05-04 00:20:22 +0530

    resolved bug in subset problem

--- src/DP/DynamicProgramming.java
+++ src/DP/DynamicProgramming.java
@@ -8,7 +8,7 @@ public class DynamicProgramming {
         int[] arr = {3, 34, 4, 12, 5, 2};
         int weight[] = {4, 5, 1};
 //        System.out.println(knapSack(4, values, weight, 3));
-        System.out.println(isSubsetSum(6, arr, 9));
+        System.out.println(isSubsetSum(6, arr, 30));
     }
 
     static int knapSack(int W, int wt[], int val[], int n) {
@@ -93,15 +93,15 @@ public class DynamicProgramming {
         for(int i =0; i < N+1; i++){
             for (int j = 0; j < sum + 1; j++) {
                 if(i == 0)
-                    t[i][j] = true;
-                if(j == 0)
                     t[i][j] = false;
+                if(j == 0)
+                    t[i][j] = true;
             }
             }
         for (int i = 1; i < N + 1; i++) {
             for (int j = 1; j < sum + 1; j++) {
                 if(arr[i-1]<= j){
-                    t[i][j] = t[i][j -arr[i-1]] || t[i-1][j];
+                    t[i][j] = t[i-1][j -arr[i-1]] || t[i-1][j];
                 }
                 else
                     t[i][j] = t[i-1][j];
