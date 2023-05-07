4b069593ae87953a04c593392e96fc2cf481acc3
Aniket <aniketmessi3@gmail.com>
2023-05-07 23:40:58 +0530

    Minimum sum partition gfg

--- src/DP/DynamicProgramming.java
+++ src/DP/DynamicProgramming.java
@@ -138,4 +138,34 @@ public class DynamicProgramming {
 
         }
     }
+    public int minDifference(int arr[], int N) {
+        // Your code goes here
+        int total = 0;
+        for(int i=0; i<N; i++) total += arr[i];
+
+        int sum = total;
+        // Subset Sum Logic !!
+        boolean[] prev = new boolean[N+1];
+        Arrays.fill(prev, true);
+
+        for(int i=0; i<N+1; i++){
+            boolean[] curr = new boolean[sum+1];
+
+            for(int j=0; j<sum+1; j++){
+                if((i==0 || i!=0) && j==0) curr[j] = true;
+                else if(i==0 && j!=0) curr[j] = false;
+                else
+                if(arr[i-1] <= j) curr[j] = prev[j] || prev[j-arr[i-1]];
+                else curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+
+        int minI = (int)Math.pow(10,9);
+        for(int s=0; s<=total/2; s++)
+            if(prev[s] == true) minI = Math.min(minI, Math.abs((total - s) - s));
+
+
+        return minI;
+    }
 }
