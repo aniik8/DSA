4cae5858ed9615223a8001f71ffffb8625712eee
Aniket <aniketmessi3@gmail.com>
2023-05-08 23:58:43 +0530

    rod cutting problem GFG

--- src/DP/UnBoundedKnapSack.java
+++ src/DP/UnBoundedKnapSack.java
@@ -2,8 +2,9 @@ package DP;
 
 public class UnBoundedKnapSack {
     public static void main(String[] args) {
-        int val[] = {1, 4, 5, 7}, wt[] = {1, 3, 4, 5};
-        System.out.println(knapSack(4, 8, val, wt));
+        int val[] = {1, 4, 5, 7}, price[] = {1, 5, 8, 9, 10, 17, 17, 20};
+//        System.out.println(knapSack(4, 8, val, wt));
+        System.out.println(cutRod(price, 8));
     }
 
     // Unbounded Knapsack - in which repetition is allowed
@@ -25,5 +26,30 @@ public class UnBoundedKnapSack {
             }
         }
         return t[N][W];
+    }
+    // Rod Cutting Problem (Exactly same as knapsack)
+    static int cutRod(int price[], int n) {
+        int[][] t= new int[n+1][n+1];
+        int[] len = new int[n];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if(i == 0 || j == 0){
+                    t[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            len[i] = i+1;
+        }
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                if(len[i-1] <= j){
+                    t[i][j] = Math.max(price[i-1] + t[i][j-len[i-1]], t[i-1][j]);
+                }
+                else t[i][j] = t[i-1][j];
+            }
+
+        }
+        return t[n][n];
     }
 }
