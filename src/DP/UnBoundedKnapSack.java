2e65c0708315be7c4b69bef75f36ece67529a508
Aniket <aniketmessi3@gmail.com>
2023-05-10 22:42:59 +0530

    coin change II / coin change gfg

--- src/DP/UnBoundedKnapSack.java
+++ src/DP/UnBoundedKnapSack.java
@@ -2,10 +2,10 @@ package DP;
 
 public class UnBoundedKnapSack {
     public static void main(String[] args) {
-        int val[] = {2}, price[] = {1, 5, 8, 9, 10, 17, 17, 20};
+        int val[] = {2,5,3,6}, price[] = {1, 5, 8, 9, 10, 17, 17, 20};
 //        System.out.println(knapSack(4, 8, val, wt));
 //        System.out.println(cutRod(price, 8));
-        System.out.println(coinChange(val, 3));
+        System.out.println(coinChange(val, 10));
     }
 
     // Unbounded Knapsack - in which repetition is allowed
@@ -54,27 +54,28 @@ public class UnBoundedKnapSack {
         }
         return t[n][n];
     }
-    // coin chabge problem
+    // coin change problem - calculate the total number of ways by which the coin can be selected to given sum
     static int coinChange(int[] coins, int amount){
         int n = coins.length;
-        int[][] t = new int[coins.length+1][amount+1];
+        int[][] t = new int[n+1][amount+1];
         for (int i = 0; i < n+1; i++) {
             for (int j = 0; j < amount+1; j++) {
-                if(j == 0)
-                    t[i][j] = 1;
-                else if(i == 0)
+                if(i == 0)
                     t[i][j] = 0;
+                else if(j == 0)
+                    t[i][j] = 1;
             }
 
         }
+        t[0][0] = 1;
         for (int i = 1; i < n+1; i++) {
             for (int j = 1; j < amount+1; j++) {
                 if(coins[i-1] <=j){
-                    t[i][j] = t[i -1][j] + t[i][j -  coins[i-1]];
+                    t[i][j] = t[i][j - coins[i-1]] + t[i-1][j];
                 }
                 else t[i][j] = t[i-1][j];
             }
         }
-        return t[n][amount] == 0 ? -1 : t[n][amount];
+        return t[n][amount];
     }
 }
