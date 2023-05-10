5f56294f5c0bf6098f33eda32d9126f09a5fb211
Aniket <aniketmessi3@gmail.com>
2023-05-11 01:26:33 +0530

    Longest common subsequence Recursive

--- src/DP/SubSequenceDP.java
+++ src/DP/SubSequenceDP.java
@@ -2,5 +2,19 @@ package DP;
 
 public class SubSequenceDP {
     public static void main(String[] args) {
+        System.out.println(longestSubsequence("abcdgh", "abedfhr"));
+    }
+    // longest common subsequence
+    static int longestSubsequence(String S, String R){
+        if(S.length() == 0 || R.length() == 0){
+            return 0;
+        }
+        else if(S.charAt(0) == R.charAt(0)){
+            return 1 + longestSubsequence(S.substring(1), R.substring(1));
+        }
+        else{
+           return Math.max(longestSubsequence(S.substring(0), R.substring(1)), longestSubsequence(S.substring(1),
+                    R.substring(0)));
+        }
     }
 }
