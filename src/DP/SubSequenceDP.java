c9bdd234a59c8f95e24fac3a8bfde5388f6ea4f1
Aniket <aniketmessi3@gmail.com>
2023-05-11 19:42:23 +0530

    Longest Subsequence Dynamic Programming

--- src/DP/SubSequenceDP.java
+++ src/DP/SubSequenceDP.java
@@ -10,7 +10,7 @@ public class SubSequenceDP {
             }
             //
         }
-        System.out.println(longestSubsequencememoize("abcdgh", "abedfhr", "abcdgh".length(),"abedfhr".length()));
+        System.out.println(longestSubsequenceDP("abcdgh", "abedfhr", "abcdgh".length(),"abedfhr".length()));
     }
     // longest common subsequence
     static int longestSubsequence(String S, String R){
@@ -42,4 +42,23 @@ public class SubSequenceDP {
                     R.substring(0, n)));
         }
     }
+    static int longestSubsequenceDP(String S, String R, int m, int n){
+        int[][] t = new int[m+1][n+1];
+        for (int i = 0; i < m+1; i++) {
+            for (int j = 0; j < n+1; j++) {
+                if(i == 0 || j == 0)
+                    t[i][j] = 0;
+            }
+        }
+       for(int i = 1; i < m+1; i++){
+           for(int j = 1; j < n+1; j++){
+               if(S.charAt(i-1) == R.charAt(j-1))
+                   t[i][j] = 1 + t[i-1][j-1];
+               else
+                   t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+           }
+       }
+       return t[m][n];
+    }
+
 }
