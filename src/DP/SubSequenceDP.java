f11b27e6238f5f6a8026278d18ac1296e0c46b5d
Aniket <aniketmessi3@gmail.com>
2023-05-15 23:06:05 +0530

    longest repeating subsequence DP

--- src/DP/SubSequenceDP.java
+++ src/DP/SubSequenceDP.java
@@ -219,5 +219,26 @@ public class SubSequenceDP {
 
         return str.reverse().toString();
     }
-    
+    // https://practice.geeksforgeeks.org/problems/longest-repeating-subsequence2004/
+    static int longestrepeatingSubsequenceDP(String S, String R, int m, int n){
+        int[][] t = new int[m+1][n+1];
+        for (int i = 0; i < m+1; i++) {
+            for (int j = 0; j < n+1; j++) {
+                if(i == 0 || j == 0)
+                    t[i][j] = 0;
+            }
+        }
+        for(int i = 1; i < m+1; i++){
+            for(int j = 1; j < n+1; j++){
+                if(S.charAt(i-1) == R.charAt(j-1) && i != j)
+                    t[i][j] = 1 + t[i-1][j-1];
+                else
+                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+            }
+        }
+        return t[m][n];
+    }
+    static int LongestRepeatingSubsequence(String str){
+        return longestrepeatingSubsequenceDP(str, str, str.length(), str.length());
+    }
 }
