6eadc74a921ae4ac9cc183ac40259c9fdadbbdd4
Aniket <aniketmessi3@gmail.com>
2023-05-18 00:03:46 +0530

    matrix chain multiplication memoize

--- src/DP/MatrixChainMultiDP.java
+++ src/DP/MatrixChainMultiDP.java
@@ -1,7 +1,13 @@
 package DP;
 
 public class MatrixChainMultiDP {
+    static int[][] t = new int[500][500];
     public static void main(String[] args) {
+        for (int i = 0; i < 500; i++) {
+            for (int j = 0; j < 500; j++) {
+                t[i][j] = -1;
+            }
+        }
         int[] arr = {10, 30, 5, 60};
         int N = 4;
         System.out.println(matrixMultiplication(N, arr));
@@ -9,7 +15,7 @@ public class MatrixChainMultiDP {
 
     static int matrixMultiplication(int N, int arr[])
     {
-        return matrixChain(1, N-1, N, arr);
+        return matrixChainMemoize(1, N-1, N, arr);
     }
     static int matrixChain(int i, int j, int N, int[] arr){
         int min = Integer.MAX_VALUE;
@@ -22,6 +28,23 @@ public class MatrixChainMultiDP {
                 min = temp;
             }
         }
+        return min;
+
+    }
+    static int matrixChainMemoize(int i, int j, int N, int[] arr){
+        int min = Integer.MAX_VALUE;
+        if(i >= j)
+            return 0;
+        if(t[i][j] != -1)
+            return t[i][j];
+        for (int k = i; k <= j-1; k++) {
+            int temp = (matrixChainMemoize(i, k, N, arr) + matrixChainMemoize(k+1, j, N, arr))
+                    + arr[i-1] * arr[j] * arr[k];
+            if(temp < min){
+                min = temp;
+            }
+        }
+        t[i][j] = min;
         return min;
 
     }
