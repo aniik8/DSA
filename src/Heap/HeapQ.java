2976b66d9d945480df626f5929eeb07752ec9c34
Aniket <aniketmessi3@gmail.com>
2023-06-04 02:26:21 +0530

    Nearly sorted (GeeksforGEEKS Q)

--- src/Heap/HeapQ.java
+++ src/Heap/HeapQ.java
@@ -4,8 +4,9 @@ import java.util.*;
 //        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 public class HeapQ {
     public static void main(String[] args) {
-      int[] arr = {3,2,3,1,2,4,5,5,6};
+      int[] arr = {3,1,4,2,5};
         System.out.println(findKthLargest(arr, 4));
+        System.out.println(nearlySorted(arr, 5, 2));
     }
     // 3,2,1,5,6,4
     // 4, 5, 6
@@ -39,5 +40,27 @@ public class HeapQ {
 
         }
         return pq.peek();
+    }
+    // Nearly sorted array
+    // {6,5,3,2,8,10,9} k = 3
+    // 2,3,5,6
+    static ArrayList <Integer> nearlySorted(int arr[], int num, int k)
+    {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = arr.length;
+        int a = 0;
+        for (int i = 0; i < n; i++) {
+            pq.add(arr[i]);
+            a++;
+            if(a > k){
+                list.add(pq.poll());
+                a--;
+            }
+        }
+        while(!pq.isEmpty()){
+            list.add(pq.poll());
+        }
+        return list;
     }
 }
