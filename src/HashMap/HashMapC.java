c917fbd1d0d0d00933a74f0790ef3f0fb2c27e4b
Aniket <aniketmessi3@gmail.com>
2023-06-04 01:40:27 +0530

    Single Number I Leetcode

--- src/HashMap/HashMapC.java
+++ src/HashMap/HashMapC.java
@@ -7,5 +7,22 @@ public class HashMapC {
         int[] arr = {1,3,1,-1,3};
         System.out.println(singleNumber(arr));
     }
-   
+    public static int singleNumber(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i], 1);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(map.get(nums[i]) == 1){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
 }
