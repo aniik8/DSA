3cfa9c41c3d2b3c3d750692a29a6879c4e1ae9e1
Aniket <aniketmessi3@gmail.com>
2023-06-04 01:41:23 +0530

    single number 3 leetcode

--- src/HashMap/HashMapC.java
+++ src/HashMap/HashMapC.java
@@ -43,4 +43,27 @@ public class HashMapC {
         }
         return nums[nums.length-1];
     }
+    // single number 3 leetcode
+    public int[] singleNumberIII(int[] nums) {
+        int n = nums.length, a = 0;
+        int[] ans = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i], 1);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(map.get(nums[i]) == 1){
+                ans[a++] = nums[i];
+                if(a > 2){
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
 }
