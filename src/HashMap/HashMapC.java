ec7314144055c9fd14873b6f5697ca302c0becb6
Aniket <aniketmessi3@gmail.com>
2023-06-04 01:40:53 +0530

    Single number II leetcode

--- src/HashMap/HashMapC.java
+++ src/HashMap/HashMapC.java
@@ -25,4 +25,22 @@ public class HashMapC {
         }
         return nums[nums.length-1];
     }
+    public int singleNumberII(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i], 1);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(map.get(nums[i]) == 1){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
 }
