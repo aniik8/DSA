6cc028fcd09b3675c6ace3dc47da534ca5fbd538
Aniket <aniketmessi3@gmail.com>
2023-06-04 02:08:46 +0530

    Kth largest element

--- src/Heap/HeapQ.java
+++ src/Heap/HeapQ.java
@@ -1,8 +1,26 @@
 package Heap;
 import java.util.HashMap;
+import java.util.*;
+//        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 public class HeapQ {
     public static void main(String[] args) {
-
+      int[] arr = {3,2,3,1,2,4,5,5,6};
+        System.out.println(findKthLargest(arr, 4));
     }
-
+    // 3,2,1,5,6,4
+    // 4, 5, 6
+    static public int findKthLargest(int[] nums, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = nums.length;
+        int a = 0;
+        for (int i = 0; i < n; i++) {
+            pq.add(nums[i]);
+            a++;
+            if(a > k){
+                pq.poll();
+                a--;
+            }
+        }
+        return pq.peek();
+    }
 }
