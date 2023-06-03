32f65ecadb53af34b358fb1eccae75692acf3fd8
Aniket <aniketmessi3@gmail.com>
2023-06-04 01:40:04 +0530

    Hashmap and Heap

---
+++ src/HashMap/HashMapC.java
@@ -0,0 +1,11 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class HashMapC {
+    public static void main(String[] args) {
+        int[] arr = {1,3,1,-1,3};
+        System.out.println(singleNumber(arr));
+    }
+   
+}
---
+++ src/Heap/HeapQ.java
@@ -0,0 +1,8 @@
+package Heap;
+import java.util.HashMap;
+public class HeapQ {
+    public static void main(String[] args) {
+
+    }
+
+}
