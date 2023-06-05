d2cd6dc2b7ca09785f50cc355662f32cb28a7cad
Aniket <aniketmessi3@gmail.com>
2023-06-05 23:53:08 +0530

    K closest elements

--- src/Heap/HeapQ.java
+++ src/Heap/HeapQ.java
@@ -4,9 +4,11 @@ import java.util.*;
 //        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 public class HeapQ {
     public static void main(String[] args) {
-      int[] arr = {3,1,4,2,5};
+      int[] arr = {12, 16, 22, 30, 35, 39, 42,
+              45, 48, 50, 53, 55, 56};
         System.out.println(findKthLargest(arr, 4));
         System.out.println(nearlySorted(arr, 5, 2));
+        System.out.println(Arrays.toString(printKClosest(arr, arr.length, 4, 35)));
     }
     // 3,2,1,5,6,4
     // 4, 5, 6
@@ -63,4 +65,36 @@ public class HeapQ {
         }
         return list;
     }
+    // Find the Kth closest elements -
+    static int[] printKClosest(int[] arr, int n, int k, int x) {
+        int[] array = new int[k];
+        int a = 0;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return Math.abs(b - x) - Math.abs(a - x);
+            }
+        });
+        for(int i = 0; i< arr.length; i++){
+            maxHeap.add(arr[i]);
+            if(maxHeap.size() > k){
+                maxHeap.poll();
+            }
+        }
+        while(!maxHeap.isEmpty()){
+            array[a++] = maxHeap.peek();
+            maxHeap.poll();
+        }
+        return array;
+    }
+    // LeetCode 658
 }
+
+
+
+
+
+
+
+
+
