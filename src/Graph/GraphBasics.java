e404b0ff49cde57e5de84b21438b2757e20a1382
Aniket <aniketmessi3@gmail.com>
2023-06-17 19:53:46 +0530

    graph using adjacency list

--- src/Graph/GraphBasics.java
+++ src/Graph/GraphBasics.java
@@ -1,10 +1,12 @@
 package Graph;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GraphBasics {
     public static void main(String[] args) {
-
+//        representMatrix();
+        addGraph();
     }
 
     // Graph using adjacency list
@@ -28,5 +30,35 @@ public class GraphBasics {
             }
             System.out.println();
         }
+    }
+    // list representation of Graph
+    static void addEdge(ArrayList<ArrayList<Integer>> adj, int n, int m){
+        adj.get(n).add(m);
+        adj.get(m).add(n);
+    }
+    static void printAdjList(ArrayList<ArrayList<Integer>> adj){
+        for (int i = 0; i < adj.size(); i++) {
+            System.out.print("Head is " + i + " ->");
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(j) + " -> " );
+            }
+            System.out.println();
+        }
+    }
+    static void addGraph(){
+        int vertices = 5;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(vertices);
+        for (int i = 0; i < vertices; i++) {
+                adj.add(new ArrayList<>());
+        }
+        addEdge(adj, 0, 1);
+        addEdge(adj, 0, 4);
+        addEdge(adj, 1, 2);
+        addEdge(adj, 1, 3);
+        addEdge(adj, 1, 4);
+        addEdge(adj, 2, 3);
+        addEdge(adj, 3, 4);
+
+        printAdjList(adj);
     }
 }
