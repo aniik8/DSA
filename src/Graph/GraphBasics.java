1d50607e8a24aa7b9b5d96db410a0e7ce3232c9a
Aniket <aniketmessi3@gmail.com>
2023-06-17 19:07:34 +0530

    matrix representation of graph

--- src/Graph/GraphBasics.java
+++ src/Graph/GraphBasics.java
@@ -1,4 +1,32 @@
 package Graph;
 
+import java.util.Scanner;
+
 public class GraphBasics {
+    public static void main(String[] args) {
+
+    }
+
+    // Graph using adjacency list
+    static void representMatrix(){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();   // number of vertices
+        int m = sc.nextInt();   // number of edges.
+        int[][] adjMatrix = new int[n+1][n+1];
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adjMatrix[u][v] = 1;
+            adjMatrix[v][u] = 1;
+        }
+        printAdjMatrix(adjMatrix, n);
+    }
+    static void printAdjMatrix(int[][] arr, int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
