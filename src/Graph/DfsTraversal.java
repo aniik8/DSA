902c41f87aa1108bbe1f984cf9957d529784800f
Aniket <aniketmessi3@gmail.com>
2023-06-19 21:42:21 +0530

    detect cycle in a graph

--- src/Graph/DfsTraversal.java
+++ src/Graph/DfsTraversal.java
@@ -42,7 +42,8 @@ public class DfsTraversal {
                 dfsTraversal.add(vertex);
 
                 ArrayList<Integer> neighbors = adj.get(vertex);
-                for (int i = neighbors.size() - 1; i >= 0; i--) {
+                for (int i = neighbors.size() - 1; i >= 0; i--)
+                {
                     int neighbor = neighbors.get(i);
                     if (!visited[neighbor]) {
                         stack.push(neighbor);
@@ -52,6 +53,38 @@ public class DfsTraversal {
         }
 
         return dfsTraversal;
+    }
+
+    // DFS traversal similar problem ---> Detect a cycle in a graph
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+
+        boolean[] visited = new boolean[V];
+
+        for (int v = 0; v < V; v++) {
+            if (!visited[v]) {
+                Stack<Integer> stack = new Stack<>();
+                stack.push(v);
+                int[] parent = new int[V];
+                parent[v] = -1;
+
+                while (!stack.isEmpty()) {
+                    int vertex = stack.pop();
+                    visited[vertex] = true;
+
+                    ArrayList<Integer> neighbors = adj.get(vertex);
+                    for (int neighbor : neighbors) {
+                        if (!visited[neighbor]) {
+                            stack.push(neighbor);
+                            parent[neighbor] = vertex;
+                        } else if (neighbor != parent[vertex]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 
 }
