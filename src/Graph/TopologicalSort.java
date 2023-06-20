aaa562d1c62d110da5fdd859a5535c637eca15fe
Aniket <aniketmessi3@gmail.com>
2023-06-20 23:42:46 +0530

    Topological sort

--- src/Graph/TopologicalSort.java
+++ src/Graph/TopologicalSort.java
@@ -1,4 +1,30 @@
 package Graph;
-
+import java.util.*;
 public class TopologicalSort {
+    public static void main(String[] args) {
+
+    }
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj)
+    {   boolean[] visited = new boolean[V];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < V; i++) {
+            if(!visited[i]){
+                dfs(i, adj, visited, stack);
+            }
+        }
+        int[] result = new int[V];
+        int x = 0;
+        for (int i = 0; i < V; i++) {
+            result[x++] = stack.pop();
+        }
+        return result;
+    }
+    static void dfs(int v, ArrayList<ArrayList<Integer>> adj,boolean[] visited, Stack<Integer>stack){
+        visited[v] = true;
+        for (int neighbor : adj.get(v)) {
+            if(!visited[neighbor])
+                dfs(neighbor, adj, visited, stack);
+        }
+        stack.push(v);
+    }
 }
