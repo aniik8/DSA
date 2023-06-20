d969df8230a3ad014509d64de552942a8f7cb944
Aniket <aniketmessi3@gmail.com>
2023-06-21 00:26:34 +0530

    topological sort using bfs(kahn's) algorithm

--- src/Graph/TopologicalSort.java
+++ src/Graph/TopologicalSort.java
@@ -29,8 +29,39 @@ public class TopologicalSort {
     }
 
     // kahn's algorithm --- aks
-
-    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj){
-
+    // this algorithm uses the topological sort using bfs where we first check the incoming degree(or indegree) of
+    // each node element. 2. now we'll see that the element who have the indegree of 0, gets added to the queue
+    // 3. until our queue is not empty we'll add the current queue front element and and we'll check it's neighbor
+    // if the neighbor have a indegree of 0 (--indegree[neighbor] == 0) then add the neighbor to the queue)
+    static int[] topoSortBFS(int V, ArrayList<ArrayList<Integer>> adj){
+        Queue<Integer> queue = new LinkedList<>();
+        int[] indegree = new int[V];
+        for(ArrayList<Integer> list: adj){
+            for (Integer e :
+                    list) {
+                indegree[e]++;
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        bfs(list, indegree, V, queue,adj);
+        int[] arr = new int[V];
+        for (int i = 0; i < V; i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+    static void bfs(ArrayList<Integer> list, int[] indegree, int V, Queue<Integer> queue,ArrayList<ArrayList<Integer>> adj){
+        for (int i = 0; i < V; i++) {
+            if(indegree[i] == 0)
+                queue.add(i);
+        }
+        while (!queue.isEmpty()){
+            int x = queue.poll();
+            list.add(x);
+            for (int a : adj.get(x)) {
+                if(--indegree[a] == 0)
+                    queue.add(a);
+            }
+        }
     }
 }
