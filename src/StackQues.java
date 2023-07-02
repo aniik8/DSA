b4f5b9e67d6ac39a378ae97a5d721d98092abfd2
Aniket <aniketmessi3@gmail.com>
2023-07-02 23:28:33 +0530

    stack ds

---
+++ src/StackQues.java
@@ -0,0 +1,10 @@
+public class StackQues {
+    public static void main(String[] args) {
+
+    }
+    // stack questions. From GFG and leetcode
+    // 1. Identification of stack
+    // inside loop value of j depends on ith loop;
+    // 1. Stack implementation using array.
+    //
+}
