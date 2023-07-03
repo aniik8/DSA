1df88d0faeb1fe2b2acd5d4380ab871aca2cc47f
Aniket <aniketmessi3@gmail.com>
2023-07-03 22:50:26 +0530

    larger element left

--- src/Stack/StackQues.java
+++ src/Stack/StackQues.java
@@ -16,8 +16,8 @@ public class StackQues {
 //        stack.push(9);
 //        System.out.println(stack.peek());
 //        stack.isEmpty();
-        long[] num = {1,3,2,4};
-        System.out.println(Arrays.toString(nextLargerElement(num, num.length)));
+        long[] num = {10, 5, 11, 6, 20, 12} ;
+        System.out.println(Arrays.toString(nextLargerElementLeft(num, num.length)));
     }
     // stack questions. From GFG and leetcode
     // 1. Identification of stack
@@ -40,6 +40,28 @@ public class StackQues {
                 }
                 if(stack.isEmpty()) array[a--] = -1;
                 else array[a--] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return array;
+    }
+    // nearest greatest to left.
+    public static long[] nextLargerElementLeft(long[] arr, int n){
+        long[] array = new long[n];
+        Stack<Long> stack = new Stack<>();
+        int a = 0;
+        for (int i = 0; i <n  ; i++) {
+            if(stack.size() == 0)
+                array[a++] = -1;
+            else if(stack.size() > 0 && arr[i] < stack.peek())
+                array[a++] = stack.peek();
+            else if(stack.size() > 0 && arr[i] >= stack.peek())
+            {
+                while(!stack.isEmpty() && arr[i] >= stack.peek()){
+                    stack.pop();
+                }
+                if(stack.isEmpty()) array[a++] = -1;
+                else array[a++] = stack.peek();
             }
             stack.push(arr[i]);
         }
