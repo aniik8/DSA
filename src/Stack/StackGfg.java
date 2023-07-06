ab0b148bfeefaab49f8b4cfdc135abef816e8742
Aniket <aniketmessi3@gmail.com>
2023-07-06 23:13:20 +0530

    is valid parenthesis

--- src/Stack/StackGfg.java
+++ src/Stack/StackGfg.java
@@ -6,24 +6,61 @@ public class StackGfg
 {
     public static void main(String[] args) {
         int M[][] = {{0,1},
-                     {1,0}};
+                     {1,0},
+                     };
         System.out.println(celebrity(M, M.length));
     }
     static int celebrity(int M[][], int n)
     {
+        System.out.println(M.length + " " + M[0].length);
+
         Stack<pair> stack = new Stack<>();
         for (int i = 0; i < M.length; i++) {
             for (int j = 0; j < M[i].length; j++) {
-                if(M[i][j] == 1)
-                    if(!stack.isEmpty())
-                    {
-                        if(stack.peek().i == i || stack.peek().j == j)
+                if(M[i][j] == 1) {
+                    if (!stack.isEmpty()) {
+                        System.out.println(stack.peek().j == j);
+                        if (stack.peek().j == j)
                             return j;
                     }
                     stack.push(new pair(i, j));
+                }
             }
         }
         return -1;
+    }
+    // is valid parenthesis
+    public boolean isValid(String s) {
+        boolean flag = true;
+        if(s.length() <= 1) return false; // for empty or string len=1;
+        Stack<Character> parenthesis = new Stack<>();
+        for(int i=0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            switch (ch)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    parenthesis.push(ch);
+                    break;
+                case ')':
+                case '}':
+                case ']':
+                    if(!parenthesis.isEmpty())
+                    {
+                        char chPop = parenthesis.pop();
+                        if ((ch == '}' && chPop != '{')
+                                || (ch == ')' && chPop != '(')
+                                || (ch == ']' && chPop != '['))
+                        {
+                            return false;
+                        }
+                    }
+                    else return false;  // prematurely empty
+            }
+        }
+        if(!parenthesis.isEmpty()) return false;
+        else return true;
     }
 }
 class pair{
