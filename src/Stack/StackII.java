ea2d59a040128a5f42ce6e0dd772917fdc459783
Aniket <aniketmessi3@gmail.com>
2023-07-07 23:34:05 +0530

    min stack II

--- src/Stack/StackII.java
+++ src/Stack/StackII.java
@@ -82,3 +82,37 @@ class ArithmeticExpressionEvaluation {
     }
 
 }
+
+
+class MinStackII {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStackII() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
