f18529563da7f7f141f8a3bb63fe238d5482e456
Aniket <aniketmessi3@gmail.com>
2023-07-16 03:21:30 +0530

    leetcode 387

--- src/HashMap/HashMapC.java
+++ src/HashMap/HashMapC.java
@@ -7,6 +7,21 @@ public class HashMapC {
     public static void main(String[] args) {
 //        int[] arr = {2,2,1};
 //        System.out.println(singleNumberr(arr));
+        System.out.println(firstUniqChar("dddccdbba"));
+    }
+    static int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0;i < s.length(); i++){
+            if(map.containsKey(s.charAt(i)))
+                map.put(s.charAt(i), map.get(s.charAt(i))+1);
+            else
+                map.put(s.charAt(i), 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if(map.get(s.charAt(i)) == 1)
+                return s.indexOf(s.charAt(i));
+        }
+        return -1;
     }
     public static int singleNumber(int[] nums) {
         int n = nums.length;
@@ -67,6 +82,7 @@ public class HashMapC {
         }
         return ans;
     }
+
 //    public boolean uniqueOccurrences(int[] arr) {
 //        HashSet
 //    }
