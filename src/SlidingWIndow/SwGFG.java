2ffbb6e8f31600f7124d7287bd7dae52cf07f1c3
Aniket <aniketmessi3@gmail.com>
2023-07-18 01:53:11 +0530

    First negative integer in every window of size k

--- src/SlidingWIndow/SwGFG.java
+++ src/SlidingWIndow/SwGFG.java
@@ -3,10 +3,9 @@ import java.util.*;
 
 public class SwGFG {
     public static void main(String[] args) {
-        int[] arr= {100, 200, 300, 400};
-        ArrayList<Integer> list= new ArrayList<>();
-        list = ;
-        System.out.println(maximumSumSubarray(2, );
+        long[] arr= {12, -1, -7, 8, -15, 30, 16, 28};
+
+        System.out.println(Arrays.toString(printFirstNegativeInteger(arr, 8, 3)));
     }
     // maximum sum subarray of size k
     static long maximumSumSubarray(int K, List<Integer> Arr,int N){
@@ -29,8 +28,32 @@ public class SwGFG {
         }
         return maxSum;
     }
+    // print First negative integer
+    // {-8, 2, 3, -6, 10};
     static public long[] printFirstNegativeInteger(long A[], int N, int K)
-    {
-
+    {   long[] negative = new long[N - K + 1];
+        int i = 0, j = 0, a = 0;
+        Queue<Long> queue = new LinkedList<>();
+        while(j < N){
+            if(A[j] < 0)
+            {
+                queue.add(A[j]);
+            }
+            if(j - i + 1 < K){
+                j++;
+            }
+            else if(j - i + 1 == K){
+                if(!queue.isEmpty()){
+                    negative[a++] = queue.peek();
+                    if(A[i] == queue.peek())
+                        queue.poll();
+                }else{
+                    negative[a++] = 0;
+                }
+                j++;
+                i++;
+            }
+        }
+        return negative;
     }
 }
