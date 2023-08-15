e82622db84d757854d8b30845b655648a4bdf8d1
Aniket <aniketmessi3@gmail.com>
2023-08-16 03:41:43 +0530

    dp 23

--- src/DP/DP2.java
+++ src/DP/DP2.java
@@ -6,113 +6,157 @@ public class DP2 {
     public static void main(String[] args) {
         int[] nums = {10, 8};
 //        System.out.println(minDifference(nums));
-        System.out.println(longestCommonSubsequence2("abcde", "ace"));
+//        System.out.println(longestCommonSubsequence2("abcde", "ace"));
+        System.out.println(longestCommonSubstr("ABCDGH"
+                , "ACDGHR", 6, 6));
     }
-    static int boundedKnapsack(int[] weight, int[] value, int W, int n){
-        if(n == 0 || W == 0)
+
+    static int boundedKnapsack(int[] weight, int[] value, int W, int n) {
+        if (n == 0 || W == 0)
             return 0;
         // if the current weight is less than the weight of knapsack
-        if(weight[n-1] <= W){
+        if (weight[n - 1] <= W) {
             // two choices take or leave
-            return Math.max(value[n-1]+ boundedKnapsack(weight, value, W - weight[n-1], n - 1),
-                    boundedKnapsack(weight, value, W, n-1));
-        }
-        else
-            return boundedKnapsack(weight, value, W, n-1);
+            return Math.max(value[n - 1] + boundedKnapsack(weight, value, W - weight[n - 1], n - 1),
+                    boundedKnapsack(weight, value, W, n - 1));
+        } else
+            return boundedKnapsack(weight, value, W, n - 1);
     }
-// memoization -- in recursion there are various repeating calls which increases the complexity of the program
+
+    // memoization -- in recursion there are various repeating calls which increases the complexity of the program
 // so to reduce the function calls and reduce the complexity of the program, use memoization
     static int[][] t = new int[1002][1002];
-    static int knapSack(int W, int[] wt, int[] val, int n){
+
+    static int knapSack(int W, int[] wt, int[] val, int n) {
         for (int i = 0; i < n; i++) {
             Arrays.fill(t[i], -1);
         }
         return knapSackhelper(W, wt, val, n, t);
     }
-    static int knapSackhelper(int W, int[] wt, int[] val, int n, int[][]t)
-    {
-        if(n == 0 || W == 0)
+
+    static int knapSackhelper(int W, int[] wt, int[] val, int n, int[][] t) {
+        if (n == 0 || W == 0)
             return 0;
-        if(t[n] [W] != -1)
+        if (t[n][W] != -1)
             return t[n][W];
-        if(wt[n-1] <= W)
-            return t[n][W] = Math.max(val[n-1]+knapSackhelper(W-wt[n-1], wt, val, n-1, t),
-                    knapSackhelper(W, wt, val, n-1, t));
-        else{
-            return t[n][W] = knapSackhelper(W, wt, val, n-1, t);
+        if (wt[n - 1] <= W)
+            return t[n][W] = Math.max(val[n - 1] + knapSackhelper(W - wt[n - 1], wt, val, n - 1, t),
+                    knapSackhelper(W, wt, val, n - 1, t));
+        else {
+            return t[n][W] = knapSackhelper(W, wt, val, n - 1, t);
         }
     }
-// Minimum subset difference problem
-    static public int minDifference(int[] arr, int n)
-    {
+
+    // Minimum subset difference problem
+    static public int minDifference(int[] arr, int n) {
         // minimum difference would be the one which will be the closest to the half one 
-    int range = 0, num=Integer.MAX_VALUE;
-    // sum of the given array
-    for (int i = 0; i < arr.length; i++) {
-        range += arr[i];
-    }
+        int range = 0, num = Integer.MAX_VALUE;
+        // sum of the given array
+        for (int i = 0; i < arr.length; i++) {
+            range += arr[i];
+        }
 
-    //just initialization of array
-    boolean[][] t = new boolean[n+1][range+1];
-    for (int i = 0; i < n+1; i++) {
-        for (int j = 0; j < range+1; j++) {
-            if(i == 0) {
-                t[i][j] = false;
-            }
-            if(j == 0) {
-                t[i][j] = true;
+        //just initialization of array
+        boolean[][] t = new boolean[n + 1][range + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < range + 1; j++) {
+                if (i == 0) {
+                    t[i][j] = false;
+                }
+                if (j == 0) {
+                    t[i][j] = true;
+                }
             }
         }
-    }
 
-    // subset sum problem code....
-    for (int i = 1; i < arr.length+1; i++) {
-        for (int j = 1; j < range+1; j++) {
-            if(arr[i - 1] <= j){
-                t[i][j] = t[i-1][j - arr[i-1]] || t[i-1][j];
+        // subset sum problem code....
+        for (int i = 1; i < arr.length + 1; i++) {
+            for (int j = 1; j < range + 1; j++) {
+                if (arr[i - 1] <= j) {
+                    t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j];
+                } else
+                    t[i][j] = t[i - 1][j];
             }
-            else
-                t[i][j] = t[i-1][j];
         }
+
+        for (int i = range / 2; i >= 0; i--) {
+            if (t[n][i]) {
+                num = i;
+                break;
+            }
+        }
+        return (range - 2 * num);
     }
 
-    for (int i = range/2; i  >= 0; i--) {
-        if(t[n][i]) {
-            num = i;
-            break;
-        }
-    }
-    return (range - 2 * num);
-    }
-   // lcs
-   static int longestCommonSubsequence(String text1, String text2) {
-        if(text1.length() == 0  ||  text2.length() == 0)
+    // lcs
+    static int longestCommonSubsequence(String text1, String text2) {
+        if (text1.length() == 0 || text2.length() == 0)
             return 0;
-        else if(text2.charAt(0) == text2.charAt(0))
+        else if (text2.charAt(0) == text2.charAt(0))
             return 1 + longestCommonSubsequence(text1.substring(1), text2.substring(1));
         return Math.max(longestCommonSubsequence(text1.substring(1), text2), longestCommonSubsequence(text1, text2.substring(1)));
-   }
-   // memoization
-    static int longestCommonSubsequence2(String text1, String text2){
-        int[][] t = new int[text1.length()+1][text2.length()+1];
+    }
+
+    // memoization
+    static int longestCommonSubsequence2(String text1, String text2) {
+        int[][] t = new int[text1.length() + 1][text2.length() + 1];
         for (int i = 0; i <= text1.length(); i++) {
             for (int j = 0; j <= text2.length(); j++) {
                 t[i][j] = -1;
             }
         }
-        return longestCommonSubsequencehelper(text1,text2, t, text1.length(), text2.length());
+        return longestCommonSubsequencehelper(text1, text2, t, text1.length(), text2.length());
     }
-   static int longestCommonSubsequencehelper(String text1, String text2, int[][] t, int m, int n) {
-       if(n == 0  || m == 0)
-           return 0;
-       if(t[m][n] != -1)
-           return t[m][n];
-       if(text1.charAt(m-1) == text2.charAt(n-1))
-           return t[m][n] = 1 + longestCommonSubsequencehelper(text1, text2,t, m-1, n-1);
-       else
-       return t[m][n] = Math.max(longestCommonSubsequencehelper(text1, text2, t, m, n-1),
-               longestCommonSubsequencehelper(text1, text2, t, m-1, n));
+
+    static int longestCommonSubsequencehelper(String text1, String text2, int[][] t, int m, int n) {
+        if (n == 0 || m == 0)
+            return 0;
+        if (t[m][n] != -1)
+            return t[m][n];
+        if (text1.charAt(m - 1) == text2.charAt(n - 1))
+            return t[m][n] = 1 + longestCommonSubsequencehelper(text1, text2, t, m - 1, n - 1);
+        else
+            return t[m][n] = Math.max(longestCommonSubsequencehelper(text1, text2, t, m, n - 1),
+                    longestCommonSubsequencehelper(text1, text2, t, m - 1, n));
 
 
-   }
+    }
+
+    static int longestCommonSubstr(String S1, String S2, int n, int m) {
+        int[][] t = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                t[i][j] = -1;
+            }
+        }
+        return substringhelper(S1, S2, n, m, t, 0);
+    }
+
+    static int substringhelper(String S1, String S2, int n, int m, int[][] t, int count) {
+
+        if (n == 0 || m == 0)
+            return 0;
+        if (t[n][m] != -1)
+            return t[n][m];
+        if (S1.charAt(n - 1) == S2.charAt(m - 1))
+             t[n][m] = substringhelper(S1, S2, n - 1, m - 1,  t, count+1);
+        else
+            t[n][m] = Math.max(count,Math.max(substringhelper(S1, S2, n, m-1, t, 0), substringhelper(S1, S2, n - 1, m ,t, 0)));
+
+        return Math.max(t[n][m], count);
+    }
+/*
+* int longestCommonSubstr(String S1, String S2, int n, int m){
+        return substringhelper(S1, S2, n, m, 0);
+       }
+    int substringhelper(String S1, String S2, int n, int m, int count){
+
+        if(n == 0 || m == 0)
+            return count;
+        if(S1.charAt(n-1) == S2.charAt(m-1))
+            return count = substringhelper(S1, S2, n-1, m-1, count+1);
+        else
+            return count = Math.max(substringhelper(S1, S2, n, m-1, 0), substringhelper(S1, S2, n-1,m, 0));
+
+    }*/
 }
